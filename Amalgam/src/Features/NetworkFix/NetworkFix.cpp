dddd79b52c4b0793b78f667ae27c9f75c47700e2
playvora <longliator.dev@gmail.com>
2026-05-30 11:04:07 +0700

    Fix

--- Amalgam/src/Features/NetworkFix/NetworkFix.cpp
+++ Amalgam/src/Features/NetworkFix/NetworkFix.cpp
@@ -6,7 +6,7 @@ void CReadPacketState::Store()
 	m_flFrameTime = I::GlobalVars->frametime;
 	m_flCurTime = I::GlobalVars->curtime;
 	m_nTickCount = I::GlobalVars->tickcount;
-	m_nServerTick = I::ClientState->m_nServerTick;
+	m_nServerTick = I::ClientState->m_ClockDriftMgr.m_nServerTick;
 	m_nDeltaTick = I::ClientState->m_nDeltaTick;
 }
 
@@ -16,7 +16,7 @@ void CReadPacketState::Restore()
 	I::GlobalVars->frametime = m_flFrameTime;
 	I::GlobalVars->curtime = m_flCurTime;
 	I::GlobalVars->tickcount = m_nTickCount;
-	I::ClientState->m_nServerTick = m_nServerTick;
+	I::ClientState->m_ClockDriftMgr.m_nServerTick = m_nServerTick;
 	I::ClientState->m_nDeltaTick = m_nDeltaTick;
 }
 

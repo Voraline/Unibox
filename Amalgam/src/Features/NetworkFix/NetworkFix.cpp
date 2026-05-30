47b4a74df3f2262384f638343ce672c4da856fda
playvora <longliator.dev@gmail.com>
2026-05-30 10:08:36 +0700

    Improve

--- Amalgam/src/Features/NetworkFix/NetworkFix.cpp
+++ Amalgam/src/Features/NetworkFix/NetworkFix.cpp
@@ -6,6 +6,8 @@ void CReadPacketState::Store()
 	m_flFrameTime = I::GlobalVars->frametime;
 	m_flCurTime = I::GlobalVars->curtime;
 	m_nTickCount = I::GlobalVars->tickcount;
+	m_nServerTick = I::ClientState->m_nServerTick;
+	m_nDeltaTick = I::ClientState->m_nDeltaTick;
 }
 
 void CReadPacketState::Restore()
@@ -14,6 +16,8 @@ void CReadPacketState::Restore()
 	I::GlobalVars->frametime = m_flFrameTime;
 	I::GlobalVars->curtime = m_flCurTime;
 	I::GlobalVars->tickcount = m_nTickCount;
+	I::ClientState->m_nServerTick = m_nServerTick;
+	I::ClientState->m_nDeltaTick = m_nDeltaTick;
 }
 
 void CNetworkFix::FixInputDelay(bool bFinalTick)
